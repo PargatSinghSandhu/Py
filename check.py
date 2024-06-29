e0d7d7f0698bf21bd17beb2eaf15022310ab8bde
Pargat Singh <sandhu.pargat07@gmail.com>
2024-06-29 00:10:50 -0600

    push

---
+++ check.py
@@ -0,0 +1,26 @@
+print("hello world")
+msg1="Pargat Singh"
+exp=5
+
+print("My name is " + str(exp))
+
+
+
+val="pop"
+print(val.upper())
+print(val.isupper())
+print(val[0])
+print(val.index("o"))
+
+#msg= input("Enter the value")
+#print(msg)
+
+list = [1,2,3,4]
+print(list[0:2])
+list[0]=12
+print(list)
+
+name=["chris", "akon", "pop"]
+
+name[-1]="i"
+print(name[2])
